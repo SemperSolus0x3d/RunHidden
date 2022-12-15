9da950043eb3ed4a1711e90a0b23b50a55299e58
SemperSolus0x3d <75125391+SemperSolus0x3d@users.noreply.github.com>
2022-12-16 01:20:59 +0200

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# RunHidden
+
+A simple program which hides its console window immediately after start and then runs the specified command. It also enables you to do some tricks like redirecting stdout to file inside Windows Task Scheduler tasks.
+
+## Usage
+
+```cmd
+RunHidden <COMMAND>
+```
+
+>**Note**
+>Don't forget to escape characters like `|`, `>`, `<` etc with `^` like that: `^|`.
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+std::wstring GetCommand(wchar_t** args, int beginIndex, int length);
+
+int wmain(int argc, wchar_t** argv)
+{
+    if (argc == 1)
+    {
+        std::wcout << L"No command given" << std::endl;
+        std::cin.get();
+        return -1;
+    }
+
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+    auto command = GetCommand(argv, 1, argc);
+
+    return _wsystem(command.c_str());
+}
+
+std::wstring GetCommand(wchar_t** args, int begin, int length)
+{
+    std::wstringstream stream;
+
+    for (int i = begin; i < length; i++)
+        if (wcschr(args[i], L' ') != NULL)
+            stream << L'\"' << args[i] << L"\" ";
+        else
+            stream << args[i] << L' ';
+
+    return stream.str();
+}
